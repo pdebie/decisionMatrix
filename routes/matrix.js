d9c83c6d46d794a2b9ed6f14bfd37c6394944e2f
Philippe Debie <philippe.debie@gmail.com>
2015-11-19 13:36:28 +0000

    search matrix

--- routes/matrix.js
+++ routes/matrix.js
@@ -24,6 +24,27 @@ function findIndexOfKeyValue(kv) {
   return _result;
 };
 
+function searchPath(thisPath) {
+  var diffLeft = [];
+  var diffRight = [];
+  var _result = {};
+  _result.found = false;
+  _result.value = {};
+  _.each(matrix.paths, function(p) {
+    if (!_result.found) {
+      diffLeft = _.difference(thisPath, p.path);
+      diffRight = _.difference(p.path, thisPath);
+      if (diffLeft.length == 0 && diffRight.length == 0) {
+        _result.found = true;
+        _result.value = p.result;
+        console.log(" ----  And the winner is: ", _result);
+      };
+    };
+  });
+  return _result;
+};
+
+
 /* ******************
     Create a Matrix
    ******************
@@ -36,6 +57,7 @@ function insertMatrixItem(obj) {
   var _id = -1;
   _id = findIndexOfKeyValue(obj);
   if (_id < 0) {
+    /* before insert new item find last id */
     var maxidobj = {};
     maxidobj = _.max(matrix.items, function(chr) {
       return chr.id;
@@ -76,7 +98,12 @@ function addRowMatrix(ka, va) {
       newPath.path.push(insertMatrixItem(pair));
     };
   });
-  matrix.paths.push(newPath);
+  
+  var zz = searchPath(newPath.path);
+  /* add only if not exists */
+  if (!zz.found) {
+    matrix.paths.push(newPath);
+  };
 };
 
 /*
@@ -107,7 +134,60 @@ function addMatrix(csvdef) {
   return _result;
 };
 
+/* ******************
+    Search in a Matrix
+   ******************
+ */
 
+function buildSearchPath(_si) {
+  var _pathForIn = [];
+  var idx = -1;
+  var result = true;
+
+  _.each(_si, function(i) {
+  	idx = findIndexOfKeyValue(i);
+    if (idx < 0) {
+      result = false;
+    }
+    else {
+  	  _pathForIn.push(idx);
+    };
+  });
+  _pathForIn = _.uniq(_pathForIn, true);
+  
+  /* if one of the items is not found path is rejected */
+  if (!result) {
+    _pathForIn = [];
+  };
+  return _pathForIn;
+};
+
+
+function buildSearchItem(query) {
+  var searchItem = [];
+  _.forIn(query, function(v, k){
+    var one_i = {};
+    one_i.k = k;
+    one_i.v = v;
+    searchItem.push(one_i);
+  });
+  return searchItem;
+};
+
+function searchMatrix(query) {
+  var si = []; /* search item */
+  var sp = []; /* search path */
+  si = buildSearchItem(query);
+  console.log(si);
+  sp = buildSearchPath(si);
+  console.log(sp);
+  return searchPath(sp);
+};
+
+/* ******************
+    Routing 
+   ******************
+ */
 
 /* GET matrix listing. */
 router.get('/', function(req, res, next) {
@@ -121,8 +201,21 @@ router.get('/:id', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
-  addMatrix(req.body.matrix.definition);
-  res.json({ result: 'ok'});
+  var _result = {};
+  _result.result = "";
+  if (!_.isUndefined(req.body.action)) {
+    if (req.body.action === "store"){
+      _result = addMatrix(req.body.definition);
+    }
+    else {
+      _result = searchMatrix(req.body.q);
+    };
+  }
+  else {
+    _result.result = "failed"
+    _result.message = "action undefined";
+  };
+  res.json(_result);
 });
 
 
