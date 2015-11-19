de421c7e325c123fa4c00ed2241f0c974422dfb2
Philippe Debie <philippe.debie@gmail.com>
2015-11-19 11:03:19 +0000

    traitement des lignes

--- routes/matrix.js
+++ routes/matrix.js
@@ -24,7 +24,14 @@ function findIndexOfKeyValue(kv) {
   return _result;
 };
 
-
+/* ******************
+    Create a Matrix
+   ******************
+ */
+ 
+/*
+  Insert a pair of kay value only if not exists
+*/
 function insertMatrixItem(obj) {
   var _id = -1;
   _id = findIndexOfKeyValue(obj);
@@ -34,7 +41,7 @@ function insertMatrixItem(obj) {
       return chr.id;
     });
     var newItem = _.clone(obj);
-    if (!_.isUndefined(maxidobj)) {
+    if (!_.isUndefined(maxidobj.id)) {
       _id = maxidobj.id + 1;
       newItem['id'] = _id;
     }
@@ -47,31 +54,57 @@ function insertMatrixItem(obj) {
   return _id;
 };
 
-
-function addMatrix(csvdef) {
+/*
+  Add a new row of many pair of keys, values and its corresponding path
+*/
+function addRowMatrix(ka, va) {
   var newPath = {};
   newPath.order = 1;
   newPath.path = [];
   newPath.result = '';
-  var rows = csvdef.split("\n");
-  var _k = rows[0];
-  var _v = rows[1];
-  var ka = _k.split(";");
-  var va = _v.split(";");
+  
   var listObject = _.object(ka, va);
-  _.each(listObject, function(v,k,i){
+  _.each(listObject, function(v, k, i){
     if (k === 'result') {
       newPath.result = v;
     }
     else {
-      var baba = {};
-      baba.k = k;
-      baba.v = v;
+      var pair = {};
+      pair.k = k;
+      pair.v = v;
 
-      newPath.path.push(insertMatrixItem(baba));
+      newPath.path.push(insertMatrixItem(pair));
     };
   });
   matrix.paths.push(newPath);
+};
+
+/*
+  Add or update a complete Matrix of many rows
+  First row header for the keys
+  Second and followers rows of values
+*/
+function addMatrix(csvdef) {
+  var _result = {};
+  _result.result = "success";
+  _result.message = "";
+  
+  var rows = csvdef.split("\n");
+  if (rows.length < 2) {
+    _result.result = "failed";
+    _result.message = "Expected at least two lines, one for keys, one or more for values";
+    
+  }
+  else {
+    var _k = _.first(rows);
+    var _ka = _k.split(";");
+    _.each(_.rest(rows), function(aRow) {
+      var _va = aRow.split(";");
+      addRowMatrix(_ka, _va);
+      
+    });
+  };
+  return _result;
 };
 
 
